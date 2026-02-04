bbc314bce3b42892868b7b60a7f7759e6dba371a
IamYeong <wjdrhkddud2@gmail.com>
2026-02-04 12:24:50 +0900

    Initial

---
+++ src/main/java/com/heuron/heuroncloud/HeuronCloudApplication.java
@@ -0,0 +1,13 @@
+package com.heuron.heuroncloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeuronCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeuronCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heuron/heuroncloud/HeuronCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heuron.heuroncloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeuronCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
