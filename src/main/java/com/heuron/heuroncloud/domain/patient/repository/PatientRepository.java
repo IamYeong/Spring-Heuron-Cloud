ad0fa700729194f1aff3a42e75d7c5694eadc1e9
IamYeong <wjdrhkddud2@gmail.com>
2026-02-09 00:15:35 +0900

    setting : lombok annotation processor 의존성 추가

--- src/main/java/com/heuron/heuroncloud/domain/patient/repository/PatientRepository.java
+++ src/main/java/com/heuron/heuroncloud/domain/patient/repository/PatientRepository.java
@@ -3,5 +3,5 @@ package com.heuron.heuroncloud.domain.patient.repository;
 import com.heuron.heuroncloud.domain.patient.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientJpaRepository extends JpaRepository<Patient, Long> {
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
--- src/main/java/com/heuron/heuroncloud/domain/patient/service/impl/PatientDomainServiceImpl.java
+++ src/main/java/com/heuron/heuroncloud/domain/patient/service/impl/PatientDomainServiceImpl.java
@@ -2,7 +2,7 @@ package com.heuron.heuroncloud.domain.patient.service.impl;
 
 import com.heuron.heuroncloud.domain.common.exception.BusinessException;
 import com.heuron.heuroncloud.domain.patient.entity.Patient;
-import com.heuron.heuroncloud.domain.patient.repository.PatientJpaRepository;
+import com.heuron.heuroncloud.domain.patient.repository.PatientRepository;
 import com.heuron.heuroncloud.domain.patient.service.PatientDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,17 +13,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class PatientDomainServiceImpl implements PatientDomainService {
 
-    private final PatientJpaRepository patientJpaRepository;
+    private final PatientRepository patientRepository;
 
     @Override
     @Transactional
     public Patient savePatient(Patient patient) {
-        return patientJpaRepository.save(patient);
+        return patientRepository.save(patient);
     }
 
     @Override
     public Patient getPatient(Long id) throws BusinessException {
-        return patientJpaRepository.findById(id)
+        return patientRepository.findById(id)
             .orElseThrow(BusinessException::new);
     }
 }
