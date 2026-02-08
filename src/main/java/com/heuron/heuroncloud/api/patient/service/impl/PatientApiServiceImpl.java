117be1f7ba9458864a81c7226120ee1eb505ea8b
IamYeong <wjdrhkddud2@gmail.com>
2026-02-09 01:03:51 +0900

    feat : API 서비스 추가, 서비스impl 클래스 가시성지시자 수정

---
+++ src/main/java/com/heuron/heuroncloud/api/patient/service/impl/PatientApiServiceImpl.java
@@ -0,0 +1,22 @@
+package com.heuron.heuroncloud.api.patient.service.impl;
+
+import com.heuron.heuroncloud.api.patient.dto.PatientSaveRequestDto;
+import com.heuron.heuroncloud.api.patient.service.PatientApiService;
+import com.heuron.heuroncloud.domain.patient.service.PatientDomainService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+class PatientApiServiceImpl implements PatientApiService {
+
+    private final PatientDomainService patientDomainService;
+
+    @Override
+    @Transactional
+    public Long savePatient(PatientSaveRequestDto dto) {
+        return null;
+    }
+}
