0b04f755f3bdda69f9fc7f59b828a344e08e2525
IamYeong <wjdrhkddud2@gmail.com>
2026-02-09 00:28:02 +0900

    feat : DTO 에 정규식 추가

--- src/main/java/com/heuron/heuroncloud/api/patient/dto/PatientSaveRequestDto.java
+++ src/main/java/com/heuron/heuroncloud/api/patient/dto/PatientSaveRequestDto.java
@@ -1,5 +1,8 @@
 package com.heuron.heuroncloud.api.patient.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,12 +14,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PatientSaveRequestDto {
 
+    @NotNull
+    @NotEmpty
     private String name;
 
+    @Pattern(regexp = "^(19[0-9][0-9]|20[0-9][0-9])-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$")
     private String birthday;
 
+    @Pattern(regexp = "^[mfMF]$")
     private String gender;
 
+    @NotNull
     private Boolean hasDisease;
 
 }
