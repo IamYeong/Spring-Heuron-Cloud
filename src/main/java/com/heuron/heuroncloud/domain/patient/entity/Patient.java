93f80fa095d0fee65ce32a1aaf88ea0435ae41c6
IamYeong <wjdrhkddud2@gmail.com>
2026-02-08 21:47:24 +0900

    feat : 필요한 어노테이션 추가

--- src/main/java/com/heuron/heuroncloud/domain/patient/entity/Patient.java
+++ src/main/java/com/heuron/heuroncloud/domain/patient/entity/Patient.java
@@ -1,13 +1,22 @@
 package com.heuron.heuroncloud.domain.patient.entity;
 
 import jakarta.persistence.*;
-import org.hibernate.annotations.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+@EntityListeners(value = AuditingEntityListener.class)
 public class Patient {
 
     @Id
